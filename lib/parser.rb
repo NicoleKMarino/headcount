a9e75b1fedd200857d0f253542253f317c263ce8
riskybiscuit <patrick.w.wentz@gmail.com>
2016-06-07 17:40:22 -0600

    parser functioning, needs testing

--- lib/parser.rb
+++ lib/parser.rb
@@ -5,7 +5,6 @@ class Parser
   def initialize
     @filename = "./data/kindergartners_in_full_day_program.csv"
     @parsed_data = Array.new
-    @participation = Hash.new
   end
 
   def open_csv
@@ -15,20 +14,36 @@ class Parser
 
   def parse_contents(opened_csv)
     opened_csv.each do |row|
-      @district = row[:location]
-      @year = row[:timeframe].to_i
-      @percent = row[:data].to_f
-      @participation.merge(participation_sort)
-      agg_data = [@district,@participation]
-      
-      @parsed_data << agg_data.group_by{@district}
+      district = row[:location]
+      year = row[:timeframe].to_i
+      percent = row[:data].to_f
+      agg_data = [district,year,percent]
+      format_check(agg_data) unless @parsed_data.empty?
+      @parsed_data << agg_data
     end
-    binding.pry
   end
 
-  def participation_sort
-    @participation[@year] = @percent.to_f
-    @participation
+  def format_check(row_data)
+    unless @parsed_data.last.first == row_data.first 
+      #do something
+      participation_format(@parsed_data.find_all{|r|r.first == @parsed_data.last.first})
+    end
+  end
+
+  def participation_format(compiled_data_by_school)
+    #AGGREGATE MATCHING SCORES
+    # Passing nested participation hash as arg
+    participation_by_school = compiled_data_by_school.reduce({}) do |result,school_data|
+      result[school_data[1]] = school_data.last
+      result
+    end
+    format_enrollment_hash(participation_by_school)
+  end
+
+  def format_enrollment_hash(nested_participation_hash)
+    binding.pry
+    #CREATE FINAL ARGS FOR ENROLLMENT OBJ
+    {:name => @parsed_data.last.first, :kindergarten_participation => nested_participation_hash}
   end
 
 end
