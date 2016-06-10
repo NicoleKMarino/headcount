47e81931360d26e499e174d642d59ba0f51909ed
Nicole Marino <nicolekmarino@hotmail.com>
2016-06-10 11:17:53 -0600

    finished iteration 1

--- lib/headcount_analyst.rb
+++ lib/headcount_analyst.rb
@@ -4,32 +4,55 @@ require "/Users/Nicole/Documents/mod1/headcount/lib/Enrollment/enrollment_reposi
 class HeadcountAnalyst
   def initialize
     @dr= DistrictRepository.new
-    @enrollment= @dr.load_data({:enrollment => {:kindergarten =>'/Users/Nicole/Documents/mod1/headcount/data/kindergartners_in_full_day_program.csv'}})
+    @enrollment_k= @dr.load_data({:enrollment => {:kindergarten =>'/Users/Nicole/Documents/mod1/headcount/data/kindergartners_in_full_day_program.csv'}})
+    # @enrollment_hs = @dr.load_data({:enrollment => {:#high school" =>'/Users/Nicole/Documents/mod1/headcount/data/kindergartners_in_full_day_program.csv'}})
   end
 
-def district(name)
-  @district=@dr.find_by_name(name)
-  find_district_average
-end
+  def district(name)
+     find_district_average(@dr.find_by_name(name))
+   end
 
-  def find_district_average
-    percents = @district.enrollment.kindergarten_participation_by_year.values
-    result= percents.reduce(:+) / percents.length
-  end
+   def find_district_average(district)
+     district.enrollment.kindergarten_participation_by_year.values.reduce(:+) / district.enrollment.kindergarten_participation_by_year.length
+   end
 
-end
+   def kindergarten_participation_rate_variation(district_name1, district_name2)
+     district1 = district(district_name1)
+     district2 = district(district_name2)
+     result = district1 / district2
+     binding.pry
+     if result < 1
+       puts "there was no significant change"
+     else
+       puts "There was a significant change"
+     end
+   end
 
-  def kindergarten_participation_rate_variation(district1,district2)
-    district1 = find_district_average(district1)
-    district2 = find_district_average(district2)
-    result = district1 / district2
-    if result > 1
-      puts "there was no significant change"
-    else
-      puts "There was a significant change"
-  end
-end
+   def find_average_by_year(district_name)
+     percents = district_name.enrollment.kindergarten_participation_by_year
+   end
 
-  # def kindergarten_participation_rate_variation_trend
-  # end
-end
+   def kindergarten_participation_rate_variation_trend(district_name1,district_name2)
+     district1 =@dr.find_by_name(district_name1)
+     district2 =@dr.find_by_name(district_name2)
+     percents1= find_average_by_year(district1)
+     percents2= find_average_by_year(district2)
+     yearly_difference = percents1.map do |key,value|
+       percents2value=percents2[key]
+       value/percents2value
+    end
+    puts yearly_difference
+   end
+
+
+   #
+  #  def kindergarten_participation_against_high_school_graduation(district_name)
+   #
+  #  end
+
+
+
+ end
+
+ ha = HeadcountAnalyst.new
+ ha.kindergarten_participation_rate_variation_trend("ACADEMY 20", "COLORADO")
