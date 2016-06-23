c2cd9a158cb12e6c55bd15b3952e36be7c9d8ef0
riskybiscuit <patrick.w.wentz@gmail.com>
2016-06-22 20:44:50 -0600

    final

--- lib/headcount_analyst.rb
+++ lib/headcount_analyst.rb
@@ -11,4 +11,54 @@ class HeadcountAnalyst
   def initialize(dr)
     @dr = dr
   end
+
+  def kindergarten_participation_rate_variation_trend(district_name1,district_name2)
+    district1 =@dr.find_by_name(district_name1)
+    district2 =@dr.find_by_name(district_name2)
+    percents1 = district1.enrollment.enrollment_data[:kindergarten]
+    percents2= district2.enrollment.enrollment_data[:kindergarten]
+    merge_results(percents1,percents2)
+  end
+
+  def merge_results(percents1,percents2)
+    result = percents1.merge(percents2){|k, a_value, b_value| a_value/b_value }
+    answer= result.each do |k, v|
+      result[k] = truncate_float(v)
+    end
+    answer
+  end
+
+  def kindergarten_participation_rate_variation(district, competing_district)
+    district_avg = kindergarten_variation(district)
+    if competing_district[:against] == "COLORADO"
+      competing_avg = kindergarten_variation("Colorado")
+    else
+      competing_avg = kindergarten_variation(competing_district[:against])
+    end
+    district_avg / competing_avg
+  end
+
+  def kindergarten_participation_against_high_school_graduation(district)
+    kinder = kindergarten_variation(district)
+    grad = grad_avg(@dr.find_by_name(district)) / state_grad_avg
+    kinder / grad 
+  end
+
+  def kindergarten_participation_correlates_with_high_school_graduation(district)
+    if district == {:for => "STATEWIDE"}
+      statewide_correlation_check
+    elsif district.is_a?(Hash) && district[:across].is_a?(Array)
+      subset_correlation_check(district)
+    else
+      single_district_correlation_check(district[:for])
+    end
+  end
+
+  def kindergarten_variation(district)
+    @state = @dr.districts.shift.last if @state.nil?
+    d_kin = @dr.find_by_name(district).enrollment.enrollment_data[:kindergarten_participation]
+    s_kin = @state.enrollment.enrollment_data[:kindergarten_participation]
+    calculate_average(d_kin) / calculate_average(s_kin) unless d_kin.empty?
+  end
+
 end
--- lib/result_formatter.rb
+++ lib/result_formatter.rb
@@ -1,41 +1,10 @@
 require 'pry'
 module ResultFormatter
-  def kindergarten_participation_rate_variation(district, competing_district)
-    district_avg = kindergarten_variation(district)
-    if competing_district[:against] == "COLORADO"
-      competing_avg = kindergarten_variation("Colorado")
-    else
-      competing_avg = kindergarten_variation(competing_district[:against])
-    end
-    district_avg / competing_avg
-  end
-
-  def kindergarten_participation_rate_variation_trend(district_name1,district_name2)
-    district1 =@dr.find_by_name(district_name1)
-    district2 =@dr.find_by_name(district_name2)
-    percents1 = district1.enrollment.enrollment_data[:kindergarten]
-    percents2= district2.enrollment.enrollment_data[:kindergarten]
-    merge_results(percents1,percents2)
-  end
-
-  def merge_results(percents1,percents2)
-    result = percents1.merge(percents2){|k, a_value, b_value| a_value/b_value }
-    answer= result.each do |k, v|
-      result[k] = truncate_float(v)
-    end
-    answer
-  end
 
   def truncate_float(float)
     unless float == nil
       (float * 1000).floor / 1000.to_f
     end
-  end
-
-  def kindergarten_participation_against_high_school_graduation(district)
-    kinder = kindergarten_variation(district)
-    grad = grad_avg(@dr.find_by_name(district)) / state_grad_avg
-    kinder / grad 
   end
 
   def kinder_grad_correlation_confirmation(kinder_grad_correlation)
@@ -46,28 +15,12 @@ module ResultFormatter
     end
   end
 
-  def kindergarten_participation_correlates_with_high_school_graduation(district)
-    if district == {:for => "STATEWIDE"}
-      statewide_correlation_check
-    elsif district.is_a?(Hash) && district[:across].is_a?(Array)
-      subset_correlation_check(district)
-    else
-      single_district_correlation_check(district[:for])
-    end
-  end
 
   def single_district_correlation_check(district)
     if kindergarten_variation(district)
       kinder_ptcptn_vs_hs_grad = kindergarten_participation_against_high_school_graduation(district)
       kinder_grad_correlation_confirmation(kinder_ptcptn_vs_hs_grad)
     end
-  end
-
-  def kindergarten_variation(district)
-    @state = @dr.districts.shift.last if @state.nil?
-    d_kin = @dr.find_by_name(district).enrollment.enrollment_data[:kindergarten_participation]
-    s_kin = @state.enrollment.enrollment_data[:kindergarten_participation]
-    calculate_average(d_kin) / calculate_average(s_kin) unless d_kin.empty?
   end
 
   def subset_correlation_check(subset)
